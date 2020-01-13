862f0e53b61e8d03d5ec1ba1954f4ddec39c9c04
MY <715711877@qq.com>
2020-01-13 21:18:30 +0800

    add class

---
+++ src/main/java/wang/ismy/spring/Main.java
@@ -0,0 +1,11 @@
+package wang.ismy.spring;
+
+/**
+ * @author MY
+ * @date 2020/1/13 21:17
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
